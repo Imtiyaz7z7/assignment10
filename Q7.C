f751c34c78af36879c559f9844f11d33203c32ef
Imtiyaz Hussain <96721822+Imtiyaz7z7@users.noreply.github.com>
2022-08-01 15:56:16 +0530

    Update Q7.C

--- Q7.C
+++ Q7.C
@@ -1,1 +1,21 @@
 #include<stdio.h>
+int factorial(int x);
+int main(){
+    int a,b,c;
+    printf("Write the number of items: ");
+    scanf("%d",&a);
+    printf("Write combinations: ");
+    scanf("%d",&b);
+    c = a-b;
+    int d = factorial(a)/(factorial(b)*factorial(c));
+    printf("total number of combinations are %d",d);
+}
+
+int factorial(int x){
+    int a = 1;
+    for (int i = x; i >= 1; i--)
+    {
+        a = a*i;
+    }
+    return a;
+}
